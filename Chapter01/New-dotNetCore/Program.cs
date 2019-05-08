d0eb4880440be4218fbdac661fe1b24a600108c9
user <fka74@yandex.ru>
2019-05-08 10:04:55 +0500

    Added vs-content in win10vm branch

---
+++ Chapter01/New-dotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace New_dotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using .NET Core");
+        }
+    }
+}

bf6efd261709c11b9d02c55978ab40dfbd43c934
kleng <fka74@yandex.ru>
2019-05-18 16:24:03 +0500

    progress in kleng

---
+++ vscode/Chapter02/Arguments/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var args1 = new string[4];
+            args1[0] = "red";
+            args1[1] = "yellow";
+            args1[2] = "50";
+            args1[3] = "10";
+            
+            Console.ForegroundColor = (ConsoleColor)
+                Enum.Parse(typeof(ConsoleColor), args1[0], true);
+            Console.BackgroundColor = (ConsoleColor)
+                Enum.Parse(typeof(ConsoleColor), args1[1], true);
+            try
+            {
+                Console.WindowWidth = int.Parse(args1[2]);
+                Console.WindowHeight = int.Parse(args1[3]);
+            }
+            catch (PlatformNotSupportedException)
+            {
+                Console.WriteLine("Cannot resize console windows. Unsupported platform.");
+            }
+                        
+            Console.WriteLine($"There are {args1.Length} arguments.");
+            foreach (string arg in args1)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
---
+++ vscode/Chapter02/Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0.1;
+            double b = 0.2;
+            double sum = a + b;
+            if (a + b == 0.3)
+            {
+                Console.WriteLine($"{a} + {b} equals 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"{a} + {b} does NOT equal 0.3");
+            }
+            Console.WriteLine($"sum of A and B equal to {sum}");
+            decimal c = 0.1M; //M oboznachaet desyatichniy literal
+            decimal d = 0.2M;
+            if (c + d == 0.3M)
+            {
+                Console.WriteLine($"{c} + {d} equals 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"{c} + {d} does NOT equal 0.3");
+            }
+            //Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+            //Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+            //Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
+        }
+    }
+}

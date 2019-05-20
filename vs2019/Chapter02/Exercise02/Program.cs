f5d9ae8bd453a2cddf73eb734eced9b442180bef
user <fka74@yandex.ru>
2019-05-20 14:08:19 +0500

    end ch02_w10vm

--- vs2019/Chapter02/Exercise02/Program.cs
+++ vs2019/Chapter02/Exercise02/Program.cs
@@ -5,19 +5,21 @@ namespace Exercise02
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            WriteLine($"sbyte {sizeof(sbyte)}, {sbyte.MinValue:N0}, {sbyte.MaxValue:N0} " +
-                $"byte {sizeof(byte)}, {byte.MinValue:N0}, {byte.MaxValue:N0} " +
-                $"short {sizeof(short)}, {short.MinValue:N0}, {short.MaxValue:N0} " +
-                $"ushort {sizeof(ushort)}, {ushort.MinValue:N0}, {ushort.MaxValue:N0} " +
-                $"int {sizeof(int)}, {int.MinValue:N0}, {int.MaxValue:N0} " +
-                $"uint {sizeof(uint)}, {uint.MinValue:N0}, {uint.MaxValue:N0} " +
-                $"long {sizeof(long)}, {long.MinValue:N0}, {long.MaxValue:N0} " +
-                $"ulong {sizeof(ulong)}, {ulong.MinValue:N0}, {ulong.MaxValue:N0} " +
-                $"float {sizeof(float)}, {float.MinValue:N0}, {float.MaxValue:N0} " +
-                $"double {sizeof(double)}, {double.MinValue:N0}, {double.MaxValue:N0} " +
-                $"decimal {sizeof(decimal)}, {decimal.MinValue:N0}, {decimal.MaxValue:N0}");
+            string[] types = {"sbyte", "byte", "short", "ushort", "int", "uint", "long",
+                "ulong", "float", "double"};
+            int[] size = {sizeof(sbyte), sizeof(byte), sizeof(short), sizeof(ushort), sizeof(int), sizeof(uint),
+                sizeof(long), sizeof(ulong), sizeof(float), sizeof(double)};
+            double[] minval = {sbyte.MinValue, byte.MinValue, short.MinValue, ushort.MinValue, int.MinValue,
+            uint.MinValue, long.MinValue, ulong.MinValue, float.MinValue, double.MinValue};
+            double[] maxval = {sbyte.MaxValue, byte.MaxValue, short.MaxValue, ushort.MaxValue, int.MaxValue,
+            uint.MaxValue, long.MaxValue, ulong.MaxValue, float.MaxValue, double.MaxValue};
+            WriteLine("{0,-8} {1,-2} {2,28} {3,30}", "Type", "Size", "Min", "Max");
+            WriteLine();
+            for (int i=0; i<types.Length; i++)
+                WriteLine("{0,-8} {1,-2} {2,30} {3,30}", types[i], size[i], minval[i], maxval[i]);
+            WriteLine("{0,-8} {1,-2} {2,30} {3,30}", "decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue);
         }
     }
 }

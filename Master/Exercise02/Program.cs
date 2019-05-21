0bd3d7b764bfeda9f5f41ffd6acd90fd89f90f77
kleng <fka74@yandex.ru>
2019-05-21 20:38:44 +0500

    master ex02

---
+++ Master/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace Master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] types = {"sbyte", "byte", "short", "ushort", "int", "uint", "long", "ulong", "float", "double", "decimal"};
+            int[] size = {sizeof(sbyte), sizeof(byte), sizeof(short), sizeof(ushort), sizeof(int), sizeof(uint), sizeof(long), sizeof(ulong), sizeof(float), sizeof(double), sizeof(decimal)};
+            double[] minval = {}; 
+            double[] maxval = {}; 
+            WriteLine(types);
+        }
+    }
+}

80e0ccb11bc92a667f5f17a1f19f487361aa6a15
user <fka74@yandex.ru>
2019-05-24 12:49:50 +0500

    pr_w10vm

---
+++ vs2019/Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string password = string.Empty;
+            int x = 0;
+            do
+            {
+                WriteLine("Enter password: ");
+                password = ReadLine();
+                x++;
+            }
+            while (password != "1331" & x!=10);
+            if (password == "1331") { WriteLine("Correct."); }
+            else { WriteLine("Too many incorrect entries."); }
+            
+            for(int y = 1; y <= 10; y++)
+            {
+                WriteLine(y);
+            }
+            
+            string[] names = { "Adam J", "Megan R", "Frank P", "Faridah M", "David S" };
+            foreach(string n in names)
+            {
+                WriteLine($"{n} has {n.Length} characters.");
+            }
+        }
+    }
+}

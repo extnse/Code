919826cf86bcf4cc13b5bfb1cf734d586d54a71c
user <fka74@yandex.ru>
2019-05-28 14:04:19 +0500

    ex03 to M

---
+++ Master/Exercise03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                checked
+                {
+                int max = 300;
+                var answer = string.Empty;
+                if(max>byte.MaxValue)
+                { 
+                    do
+                    {
+                        WriteLine("Warning: Possible overflow. Continue anyway? y/n");
+                        answer = ReadLine();
+                    }
+                    while (answer != "y");
+                }
+                for(byte i = 0; i<max; i++)
+                { WriteLine(i); }
+                }
+            }
+            catch(OverflowException)
+            {
+                WriteLine("Type overflow exception.");
+            }
+            catch(Exception ex)
+            {
+                WriteLine($"{ex.GetType()} - {ex.Message}");
+            }
+            // END
+        }
+    }
+}

8df9b3ff26e3d2b29628bd1f95666140bba73e66
kleng <fka74@yandex.ru>
2019-06-23 14:05:55 +0500

    ch04

--- vscode/Chapter04/WritingFunctions/Program.cs
+++ vscode/Chapter04/WritingFunctions/Program.cs
@@ -26,9 +26,68 @@ namespace WritingFunctions
                 WriteLine("You did not enter a valid number!");
             }
         }
+
         static void Main(string[] args)
         {
-            RunTimesTable();
+            //RunTimesTable();
+            RunSalesTax();
+        }
+
+        static decimal SalesTax(decimal amount, string twoLetterRegionCode)
+        {
+            decimal rate = 0.0M;
+            switch (twoLetterRegionCode)
+            {
+                case "CH": // Швейцария
+                    rate = 0.08M;
+                    break;
+                case "DK": // Дания
+                case "NO": // Норвегия
+                    rate = 0.25M;
+                    break;
+                case "GB": // Великобритания
+                case "FR": // Франция
+                    rate = 0.2M;
+                    break;
+                case "HU": // Венгрия
+                    rate = 0.27M;
+                    break;
+                case "OR": // Орегон
+                case "AK": // Аляска
+                case "MT": // Монтана
+                    rate = 0.0M;
+                    break;
+                case "ND": // Северная Дакота
+                case "WI": // Висконсин
+                case "ME": // Мэриленд
+                case "VA": // Вирджиния
+                    rate = 0.05M;
+                    break;
+                case "CA": // Калифорния
+                    rate = 0.0825M;
+                    break;
+                default: // большинство штатов США
+                    rate = 0.06M;
+                    break;
+            }
+            return amount * rate;
+        }
+
+        static void RunSalesTax()
+        {
+            Write("Enter an amount: ");
+            string amountInText = ReadLine();
+            Write("Enter a two letter region code: ");
+            string region = ReadLine();
+            if (decimal.TryParse(amountInText, out decimal amount))
+            {
+                decimal taxToPay = SalesTax(amount, region);
+                WriteLine($"You must pay {taxToPay} in sales tax.");
+            }
+            else
+            {
+                WriteLine("You did not enter a valid amount!");
+            }
         }
     }
 }

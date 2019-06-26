0c71254284158a084da5aaa72b15f170ea9d1f1c
user <fka74@yandex.ru>
2019-06-26 13:01:16 +0500

    ch04

--- vs2019/Chapter04/WritingFunctions/Program.cs
+++ vs2019/Chapter04/WritingFunctions/Program.cs
@@ -5,6 +5,18 @@ namespace WritingFunctions
 {
     class Program
     {
+        
+        // Functions' launcher
+        static void Main(string[] args)
+        {
+            //RunTimesTable();
+            //RunSalesTax();
+            //RunCardinalToOrdinal();
+            RunFactorial();
+        }
+
+        
+        // Multiply Table
         static void TimesTable(byte number)
         {
             WriteLine($"This is the {number} times table");
@@ -13,7 +25,6 @@ namespace WritingFunctions
                 WriteLine($"{row} x {number} = {row * number}");
             }
         }
-
         static void RunTimesTable()
         {
             Write("Enter a number between 0 and 255: ");
@@ -27,16 +38,14 @@ namespace WritingFunctions
             }
         }
 
-        static void Main(string[] args)
-        {
-            // RunTimesTable();
-            RunSalesTax();
-        }
         
-        static decimal SalesTax(decimal amount, string twoLetterRegionCode)
+        // Function returning value
+        static decimal SalesTax(decimal sum, string rcode)
         {
+            WriteLine(sum);
+            WriteLine(rcode);
             decimal rate = 0.0M;
-            switch(twoLetterRegionCode)
+            switch(rcode)
             {
                 case "CH": // Switzerland
                     rate = 0.08M;
@@ -70,23 +79,103 @@ namespace WritingFunctions
                     rate = 0.06M;
                     break;
             }
-            return amount * rate;
+            return sum * rate;
         }
-
         static void RunSalesTax()
         {
             Write("Enter an amount: ");
             string amountInText = ReadLine();
-            Write("Enter a two letter region code: ");
+            Write("Enter a region code in two capital letters: ");
             string region = ReadLine();
-            if (decimal.TryParse(amountInText, out decimal amount))
+            if (decimal.TryParse(amountInText, out decimal amountInNumber))
             {
-                decimal taxToPay = SalesTax(amount, region);
+                decimal taxToPay = SalesTax(amountInNumber, region);
                 WriteLine($"You must pay {taxToPay} in sales tax.");
             }
             else
             {
                 WriteLine("You did not enter a valid amount!");
+            }
+        }
+
+        
+        // Formatting input numbers
+        static string CardinalToOrdinal(int number)
+        {
+            switch(number)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return $"{number}th";
+                default:
+                    string numberAsText = number.ToString();
+                    char lastDigit = numberAsText[numberAsText.Length - 1];
+                    string suffix = String.Empty;
+                    switch(lastDigit)
+                    {
+                        case '1':
+                            suffix = "st";
+                            break;
+                        case '2':
+                            suffix = "nd";
+                            break;
+                        case '3':
+                            suffix = "rd";
+                            break;
+                        default:
+                            suffix = "th";
+                            break;
+                    }
+                    return $"{number}{suffix}";
+            }
+        }
+        static void RunCardinalToOrdinal()
+        {
+            for(int number = 1; number <=30; number++)
+            {
+                Write($"{CardinalToOrdinal(number)} ");
+            }
+        }
+
+        
+        // Factorial using recursion
+        static decimal Factorial(decimal number)
+        {
+            if(number < 1)
+            {
+                return 0;
+            }
+            else if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+        static void RunFactorial()
+        {
+            try
+            {
+                checked
+                {
+                    Write("Enter a number: ");
+                    if (int.TryParse(ReadLine(), out int number))
+                    {
+                        WriteLine($"{number:N0}! = {Factorial(number):N0}");
+                        WriteLine($"MV = {decimal.MaxValue:N0}");
+                    }
+                    else
+                    {
+                        WriteLine("You did not enter a valid number!");
+                    }
+                }
+            }
+            catch
+            {
+                WriteLine("Number too large.");
             }
         }
     }

f441f2576bd8e5fdd626ecda3ea80eaf82e85f72
extnse <fka74@yandex.ru>
2019-10-24 11:17:51 +0500

    WrFunc-CtO_added

--- vscode/Chapter04/WritingFunctions/Program.cs
+++ vscode/Chapter04/WritingFunctions/Program.cs
@@ -39,7 +39,8 @@ namespace WritingFunctions
         static void Main(string[] args)
         {
             //RunTimesTable();
-            RunSalesTax();
+            //RunSalesTax();
+            RunCardinalToOrdinal();
         }
 
         static decimal SalesTax(decimal amount, string twoLetterRegionCode)
@@ -98,5 +99,46 @@ namespace WritingFunctions
                 WriteLine("You did not enter a valid amount!");
             }
         }
+
+        static string CardinalToOrdinal(int number)
+        {
+            switch (number)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return $"{number}th";
+                default:
+                    string numberAsText = number.ToString();
+                    char lastDigit = numberAsText[numberAsText.Length - 1];
+                    string suffix = string.Empty;
+                    switch(lastDigit)
+                    {
+                        case '1':
+                            suffix = "st";
+                            break;
+                        case '2':
+                            suffix = "nd";
+                            break;
+                        case '3':
+                            suffix = "rd";
+                            break;
+                        default:
+                            suffix = "th";
+                            break;
+                    }
+                    return $"{number}{suffix}";
+            }
+        }
+
+        static void RunCardinalToOrdinal()
+        {
+            for (int number = 1; number <=40; number++)
+            {
+                Write($"{CardinalToOrdinal(number)} ");
+            }
+        }
+
+
     }
 }
